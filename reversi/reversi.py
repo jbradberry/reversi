59a3abfe33e34af2f76969f20479f35fbbe8ec58
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-01-02 15:25:33 -0500

    Added Reversi board and tests.

---
+++ reversi/reversi.py
@@ -0,0 +1,376 @@
+import re
+import string
+
+
+class Board(object):
+    num_players = 2
+    rows = cols = 8
+
+    positions = {}
+
+    str_pieces = {0: "   ", 1: " x ", 2: " o "}
+    unicode_pieces = {0: "   ", 1: u" \u25cf ", 2: u" \u25cb "}
+
+    moveRE = re.compile(r'([a-h])([1-8])')
+
+    def __init__(self, *args, **kwargs):
+        if not self.positions:
+            self.initialize()
+
+    @classmethod
+    def initialize(cls):
+        cls.positions.update(((r, c), 1 << (cls.cols * r + c))
+                             for r in xrange(cls.rows)
+                             for c in xrange(cls.cols))
+
+    def start(self):
+        # p1 placed, p2 placed, player to move
+        return (self.positions[(3,4)] + self.positions[(4,3)],
+                self.positions[(3,3)] + self.positions[(4,4)], 1)
+
+    def display(self, state, _unicode=True):
+        pieces = self.unicode_pieces if _unicode else self.str_pieces
+
+        p1_placed, p2_placed, player = state
+
+        row_sep = "  |" + "-"*(4*self.cols - 1) + "|\n"
+        header = " "*4 + "   ".join(string.lowercase[:self.cols]) + "\n"
+        msg = "Player {0} to move.\n".format(player)
+
+        P = [[0 for c in xrange(self.cols)] for r in xrange(self.rows)]
+        for (r, c), v in self.positions.iteritems():
+            if v & p1_placed:
+                P[r][c] = 1
+            elif v & p2_placed:
+                P[r][c] = 2
+
+        board = row_sep.join("%d |"%(i+1) + "|".join(pieces[x] for x in row) +
+                             "|\n" for i, row in enumerate(P))
+        board = ''.join((header, row_sep, board, row_sep, header, msg))
+        return board
+
+    def is_legal(self, state, play):
+        plays = set(self.legal_plays(state))
+        return play in plays
+
+    def next_player(self, state):
+        return state[-1]
+
+    def legal_plays(self, state):
+        ## Kogge-Stone algorithm
+        p1_placed, p2_placed, player = state
+        occupied = p1_placed | p2_placed
+        empty = 0xffffffffffffffff ^ occupied
+
+        mask_a = 0xfefefefefefefefe
+        mask_h = 0x7f7f7f7f7f7f7f7f
+
+        mine = p1_placed if player == 1 else p2_placed
+        opp = p2_placed if player == 1 else p1_placed
+        legal = 0
+
+        # N
+        g, p = mine, opp
+        g |= p & (g >> 8)
+        p &= (p >> 8)
+        g |= p & (g >> 16)
+        p &= (p >> 16)
+        g |= p & (g >> 32)
+        legal |= ((g & ~mine) >> 8) & empty
+
+        # S
+        g, p = mine, opp
+        g |= p & (g << 8)
+        p &= (p << 8)
+        g |= p & (g << 16)
+        p &= (p << 16)
+        g |= p & (g << 32)
+        legal |= ((g & ~mine) << 8) & empty
+
+        # E
+        g, p = mine, opp & mask_a
+        g |= p & (g << 1)
+        p &= (p << 1)
+        g |= p & (g << 2)
+        p &= (p << 2)
+        g |= p & (g << 4)
+        legal |= ((g & ~mine & mask_h) << 1) & empty
+
+        # W
+        g, p = mine, opp & mask_h
+        g |= p & (g >> 1)
+        p &= (p >> 1)
+        g |= p & (g >> 2)
+        p &= (p >> 2)
+        g |= p & (g >> 4)
+        legal |= ((g & ~mine & mask_a) >> 1) & empty
+
+        # NE
+        g, p = mine, opp & mask_a
+        g |= p & (g >> 7)
+        p &= (p >> 7)
+        g |= p & (g >> 14)
+        p &= (p >> 14)
+        g |= p & (g >> 28)
+        legal |= ((g & ~mine & mask_h) >> 7) & empty
+
+        # NW
+        g, p = mine, opp & mask_h
+        g |= p & (g >> 9)
+        p &= (p >> 9)
+        g |= p & (g >> 18)
+        p &= (p >> 18)
+        g |= p & (g >> 36)
+        legal |= ((g & ~mine & mask_a) >> 9) & empty
+
+        # SE
+        g, p = mine, opp & mask_a
+        g |= p & (g << 9)
+        p &= (p << 9)
+        g |= p & (g << 18)
+        p &= (p << 18)
+        g |= p & (g << 36)
+        legal |= ((g & ~mine & mask_h) << 9) & empty
+
+        # SW
+        g, p = mine, opp & mask_h
+        g |= p & (g << 7)
+        p &= (p << 7)
+        g |= p & (g << 14)
+        p &= (p << 14)
+        g |= p & (g << 28)
+        legal |= ((g & ~mine & mask_a) << 7) & empty
+
+        return [(r, c) for (r, c), v in self.positions.iteritems()
+                if v & legal]
+
+    def _legal_plays(self, state):
+        ## Alternative algorithm 2
+
+        p1_placed, p2_placed, player = state
+        occupied = p1_placed | p2_placed
+
+        mine = p1_placed if player == 1 else p2_placed
+        opp = p2_placed if player == 1 else p1_placed
+
+        plays = []
+
+        for (r, c), v in self.positions.iteritems():
+            if v & occupied:
+                continue
+            for dr, dc in ((-1, 0), (-1, 1), (0, 1), (1, 1),
+                           (1, 0), (1, -1), (0, -1), (-1, -1)):
+                cr, cc, count = r + dr, c + dc, 0
+                while ((cr, cc) in self.positions and
+                       self.positions[(cr, cc)] & opp):
+                    cr += dr
+                    cc += dc
+                    count += 1
+                if count == 0:
+                    continue
+                if ((cr, cc) in self.positions and
+                    self.positions[(cr, cc)] & mine):
+                    plays.append((r, c))
+                    break
+
+        return plays
+
+        ## Alternative algorithm 1
+
+        # p1_pieces = set((r, c) for (r, c), v in self.positions.iteritems()
+        #                 if v & p1_placed)
+        # p2_pieces = set((r, c) for (r, c), v in self.positions.iteritems()
+        #                 if v & p2_placed)
+
+        # mine = p1_pieces if player == 1 else p2_pieces
+        # opp = p2_pieces if player == 1 else p1_pieces
+        # plays = set()
+
+        # for r, c in mine:
+        #     for dr, dc in ((-1, 0), (-1, 1), (0, 1), (1, 1),
+        #                    (1, 0), (1, -1), (0, -1), (-1, -1)):
+        #         cr, cc, count = r + dr, c + dc, 0
+        #         while (cr, cc) in opp:
+        #             cr += dr
+        #             cc += dc
+        #             count += 1
+        #         if count == 0:
+        #             continue
+        #         if (cr, cc) in self.positions and (cr, cc) not in mine:
+        #             plays.add((cr, cc))
+
+        # return tuple(plays)
+
+    def winner(self, state_lst):
+        state = state_lst[-1]
+        p1_placed, p2_placed, player = state
+
+        if p2_placed == 0:
+            return 1
+        if p1_placed == 0:
+            return 2
+
+        occupied = p1_placed | p2_placed
+        if (occupied == (1 << (self.rows * self.cols)) - 1 or
+            not self.legal_plays(state)):
+            p1_score = bin(p1_placed).count('1')
+            p2_score = bin(p2_placed).count('1')
+            if p1_score > p2_score:
+                return 1
+            if p2_score > p1_score:
+                return 2
+            if p1_score == p2_score:
+                return 3
+        return 0
+
+    def winner_message(self, winner):
+        if winner == 3:
+            return "Stalemate."
+        return "Winner: Player {0}.".format(winner)
+
+    def parse(self, play):
+        result = self.moveRE.match(play)
+        if not result:
+            return
+        c, r = result.groups()
+        return int(r) - 1, 'abcdefgh'.index(c)
+
+    def play(self, state, play):
+        P = self.positions[play]
+        p1_placed, p2_placed, player = state
+
+        occupied = p1_placed | p2_placed
+        empty = 0xffffffffffffffff ^ occupied
+
+        mask_a = 0xfefefefefefefefe
+        mask_h = 0x7f7f7f7f7f7f7f7f
+
+        mine = p1_placed if player == 1 else p2_placed
+        opp = p2_placed if player == 1 else p1_placed
+        flips = 0
+
+        # N
+        g, p = P, opp
+        g |= p & (g >> 8)
+        p &= (p >> 8)
+        g |= p & (g >> 16)
+        p &= (p >> 16)
+        g |= p & (g >> 32)
+        if (g >> 8) & mine:
+            flips |= g
+
+        # S
+        g, p = P, opp
+        g |= p & (g << 8)
+        p &= (p << 8)
+        g |= p & (g << 16)
+        p &= (p << 16)
+        g |= p & (g << 32)
+        if (g << 8) & mine:
+            flips |= g
+
+        # E
+        g, p = P, opp & mask_a
+        g |= p & (g << 1)
+        p &= (p << 1)
+        g |= p & (g << 2)
+        p &= (p << 2)
+        g |= p & (g << 4)
+        if (g << 1) & mask_a & mine:
+            flips |= g
+
+        # W
+        g, p = P, opp & mask_h
+        g |= p & (g >> 1)
+        p &= (p >> 1)
+        g |= p & (g >> 2)
+        p &= (p >> 2)
+        g |= p & (g >> 4)
+        if (g >> 1) & mask_h & mine:
+            flips |= g
+
+        # NE
+        g, p = P, opp & mask_a
+        g |= p & (g >> 7)
+        p &= (p >> 7)
+        g |= p & (g >> 14)
+        p &= (p >> 14)
+        g |= p & (g >> 28)
+        if (g >> 7) & mask_a & mine:
+            flips |= g
+
+        # NW
+        g, p = P, opp & mask_h
+        g |= p & (g >> 9)
+        p &= (p >> 9)
+        g |= p & (g >> 18)
+        p &= (p >> 18)
+        g |= p & (g >> 36)
+        if (g >> 9) & mask_h & mine:
+            flips |= g
+
+        # SE
+        g, p = P, opp & mask_a
+        g |= p & (g << 9)
+        p &= (p << 9)
+        g |= p & (g << 18)
+        p &= (p << 18)
+        g |= p & (g << 36)
+        if (g << 9) & mask_a & mine:
+            flips |= g
+
+        # SW
+        g, p = P, opp & mask_h
+        g |= p & (g << 7)
+        p &= (p << 7)
+        g |= p & (g << 14)
+        p &= (p << 14)
+        g |= p & (g << 28)
+        if (g << 7) & mask_h & mine:
+            flips |= g
+
+        mine |= flips
+        opp &= ~flips
+        p1_placed = mine if player == 1 else opp
+        p2_placed = opp if player == 1 else mine
+
+        if self.legal_plays((p1_placed, p2_placed, 3-player)):
+            return (p1_placed, p2_placed, 3-player)
+        return (p1_placed, p2_placed, player)
+
+    def _play(self, state, play):
+        R, C = play
+        p1_placed, p2_placed, player = state
+
+        p1_pieces = set((r, c) for (r, c), v in self.positions.iteritems()
+                        if v & p1_placed)
+        p2_pieces = set((r, c) for (r, c), v in self.positions.iteritems()
+                        if v & p2_placed)
+
+        mine = p1_pieces if player == 1 else p2_pieces
+        opp = p2_pieces if player == 1 else p1_pieces
+        flips = set()
+
+        for dr, dc in ((-1, 0), (-1, 1), (0, 1), (1, 1),
+                       (1, 0), (1, -1), (0, -1), (-1, -1)):
+            cr, cc, marked = R + dr, C + dc, set()
+            while (cr, cc) in opp:
+                marked.add((cr, cc))
+                cr += dr
+                cc += dc
+            if (cr, cc) in mine:
+                flips |= marked
+
+        mine.add((R, C))
+        mine |= flips
+        opp -= flips
+
+        p1_pieces = mine if player == 1 else opp
+        p2_pieces = opp if player == 1 else mine
+
+        p1_placed = sum(self.positions[(r, c)] for r, c in p1_pieces)
+        p2_placed = sum(self.positions[(r, c)] for r, c in p2_pieces)
+
+        if self.legal_plays((p1_placed, p2_placed, 3-player)):
+            player = 3 - player
+        return (p1_placed, p2_placed, player)
---
+++ reversi/tests.py
@@ -0,0 +1,327 @@
+import unittest2
+from reversi import Board
+
+board = Board()
+
+
+class LegalMoves(unittest2.TestCase):
+    def test_north_simple(self):
+        p1 = sum(board.positions[x] for x in [(5,3)])
+        p2 = sum(board.positions[x] for x in [(2,3), (3,3), (4,3)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [(1,3)])
+
+    def test_west_simple(self):
+        p1 = sum(board.positions[x] for x in [(3,2)])
+        p2 = sum(board.positions[x] for x in [(3,3), (3,4), (3,5)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [(3,6)])
+
+    def test_south_simple(self):
+        p1 = sum(board.positions[x] for x in [(2,5)])
+        p2 = sum(board.positions[x] for x in [(3,5), (4,5), (5,5)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [(6,5)])
+
+    def test_east_simple(self):
+        p1 = sum(board.positions[x] for x in [(4,6)])
+        p2 = sum(board.positions[x] for x in [(4,3), (4,4), (4,5)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [(4,2)])
+
+    def test_northeast_simple(self):
+        p1 = sum(board.positions[x] for x in [(5,5)])
+        p2 = sum(board.positions[x] for x in [(2,2), (3,3), (4,4)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [(1,1)])
+
+    def test_northwest_simple(self):
+        p1 = sum(board.positions[x] for x in [(5,2)])
+        p2 = sum(board.positions[x] for x in [(4,3), (3,4), (2,5)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [(1,6)])
+
+    def test_southwest_simple(self):
+        p1 = sum(board.positions[x] for x in [(2,2)])
+        p2 = sum(board.positions[x] for x in [(3,3), (4,4), (5,5)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [(6,6)])
+
+    def test_southeast_simple(self):
+        p1 = sum(board.positions[x] for x in [(2,5)])
+        p2 = sum(board.positions[x] for x in [(3,4), (4,3), (5,2)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [(6,1)])
+
+    def test_no_simple_west_wraps(self):
+        p1 = sum(board.positions[x] for x in [(3,7)])
+        p2 = sum(board.positions[x] for x in [(4,0), (4,1)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [])
+
+    def test_no_simple_east_wraps(self):
+        p1 = sum(board.positions[x] for x in [(4,0)])
+        p2 = sum(board.positions[x] for x in [(3,6), (3,7)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [])
+
+    def test_no_simple_northeast_wraps(self):
+        p1 = sum(board.positions[x] for x in [(4,0)])
+        p2 = sum(board.positions[x] for x in [(1,6), (2,7)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [])
+
+    def test_no_simple_northwest_wraps(self):
+        p1 = sum(board.positions[x] for x in [(3,7)])
+        p2 = sum(board.positions[x] for x in [(3,0), (2,1)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [])
+
+    def test_no_simple_southwest_wraps(self):
+        p1 = sum(board.positions[x] for x in [(3,7)])
+        p2 = sum(board.positions[x] for x in [(5,0), (6,1)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [])
+
+    def test_no_simple_southeast_wraps(self):
+        p1 = sum(board.positions[x] for x in [(4,0)])
+        p2 = sum(board.positions[x] for x in [(3,6), (4,7)])
+        self.assertEqual(board.legal_plays((p1, p2, 1)), [])
+
+    def test_unknown_problem(self):
+        p1 = sum(board.positions[x] for x in
+                 [(0,4), (1,1), (1,2), (1,3), (1,4), (1,5), (2,1), (2,2),
+                  (2,3), (2,4), (2,5), (2,6), (3,1), (3,2), (3,3), (3,4),
+                  (3,5), (3,6), (4,0), (4,4), (4,6), (4,7), (5,5), (5,6),
+                  (6,4), (6,5), (6,6), (7,5), (7,7)])
+        p2 = sum(board.positions[x] for x in
+                 [(0,5), (2,0), (2,7), (4,2), (4,3), (4,5), (5,2), (5,4),
+                  (6,1), (6,2), (6,3), (7,0), (7,2)])
+        self.assertEqual(set(board.legal_plays((p1, p2, 2))),
+                         set([(0,1), (0,2), (0,3), (0,6), (1,0), (1,6),
+                              (4,1), (5,7), (6,7), (7,4), (7,6)]))
+
+
+class Plays(unittest2.TestCase):
+    def test_simple_north_capture(self):
+        p1 = sum(board.positions[x] for x in [(5,3)])
+        p2 = sum(board.positions[x] for x in [(2,3), (3,3), (4,3)])
+        p1_new = p1 | p2 | board.positions[(1,3)]
+        self.assertEqual(board.play((p1, p2, 1), (1, 3)),
+                         (p1_new, 0, 1))
+
+    def test_simple_west_capture(self):
+        p1 = sum(board.positions[x] for x in [(3,2)])
+        p2 = sum(board.positions[x] for x in [(3,3), (3,4), (3,5)])
+        p1_new = p1 | p2 | board.positions[(3,6)]
+        self.assertEqual(board.play((p1, p2, 1), (3, 6)),
+                         (p1_new, 0, 1))
+
+    def test_simple_south_capture(self):
+        p1 = sum(board.positions[x] for x in [(5,3)])
+        p2 = sum(board.positions[x] for x in [(2,3), (3,3), (4,3)])
+        p1_new = p1 | p2 | board.positions[(1,3)]
+        self.assertEqual(board.play((p1, p2, 1), (1, 3)),
+                         (p1_new, 0, 1))
+
+    def test_simple_east_capture(self):
+        p1 = sum(board.positions[x] for x in [(2,5)])
+        p2 = sum(board.positions[x] for x in [(3,5), (4,5), (5,5)])
+        p1_new = p1 | p2 | board.positions[(6,5)]
+        self.assertEqual(board.play((p1, p2, 1), (6, 5)),
+                         (p1_new, 0, 1))
+
+    def test_simple_northeast_capture(self):
+        p1 = sum(board.positions[x] for x in [(5,5)])
+        p2 = sum(board.positions[x] for x in [(2,2), (3,3), (4,4)])
+        p1_new = p1 | p2 | board.positions[(1,1)]
+        self.assertEqual(board.play((p1, p2, 1), (1, 1)),
+                         (p1_new, 0, 1))
+
+    def test_simple_northwest_capture(self):
+        p1 = sum(board.positions[x] for x in [(5,2)])
+        p2 = sum(board.positions[x] for x in [(4,3), (3,4), (2,5)])
+        p1_new = p1 | p2 | board.positions[(1,6)]
+        self.assertEqual(board.play((p1, p2, 1), (1, 6)),
+                         (p1_new, 0, 1))
+
+    def test_simple_southwest_capture(self):
+        p1 = sum(board.positions[x] for x in [(2,2)])
+        p2 = sum(board.positions[x] for x in [(3,3), (4,4), (5,5)])
+        p1_new = p1 | p2 | board.positions[(6,6)]
+        self.assertEqual(board.play((p1, p2, 1), (6, 6)),
+                         (p1_new, 0, 1))
+
+    def test_simple_southeast_capture(self):
+        p1 = sum(board.positions[x] for x in [(2,5)])
+        p2 = sum(board.positions[x] for x in [(3,4), (4,3), (5,2)])
+        p1_new = p1 | p2 | board.positions[(6,1)]
+        self.assertEqual(board.play((p1, p2, 1), (6, 1)),
+                         (p1_new, 0, 1))
+
+    def test_no_north_wrapped_capture(self):
+        p1 = sum(board.positions[x] for x in [(3,4)])
+        p2 = sum(board.positions[x] for x in [(1,4), (2,4)])
+
+        x1 = board.positions[(6,3)]
+        x2 = board.positions[(7,3)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0, 4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 4)),
+                         (p1 | p2 | x1 | board.positions[(0,4)], x2, 2))
+
+        x1 = board.positions[(6,4)]
+        x2 = board.positions[(7,4)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0, 4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 4)),
+                         (p1 | p2 | x1 | board.positions[(0,4)], x2, 2))
+
+        x1 = board.positions[(6,5)]
+        x2 = board.positions[(7,5)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0,4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 4)),
+                         (p1 | p2 | x1 | board.positions[(0,4)], x2, 2))
+
+    def test_no_west_wrapped_capture(self):
+        p1 = sum(board.positions[x] for x in [(4,4)])
+        p2 = sum(board.positions[x] for x in [(4,5), (4,6)])
+
+        x1 = board.positions[(3,1)]
+        x2 = board.positions[(3,0)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(4,7)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (4, 7)),
+                         (p1 | p2 | x1 | board.positions[(4,7)], x2, 2))
+
+        x1 = board.positions[(4,1)]
+        x2 = board.positions[(4,0)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(4,7)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (4, 7)),
+                         (p1 | p2 | x1 | board.positions[(4,7)], x2, 2))
+
+        x1 = board.positions[(5,1)]
+        x2 = board.positions[(5,0)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(4,7)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (4, 7)),
+                         (p1 | p2 | x1 | board.positions[(4,7)], x2, 2))
+
+    def test_no_south_wrapped_capture(self):
+        p1 = sum(board.positions[x] for x in [(4,3)])
+        p2 = sum(board.positions[x] for x in [(5,3), (6,3)])
+
+        x1 = board.positions[(1,2)]
+        x2 = board.positions[(0,2)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 3)),
+                         (p1 | p2 | x1 | board.positions[(7,3)], x2, 2))
+
+        x1 = board.positions[(1,3)]
+        x2 = board.positions[(0,3)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 3)),
+                         (p1 | p2 | x1 | board.positions[(7,3)], x2, 2))
+
+        x1 = board.positions[(1,4)]
+        x2 = board.positions[(0,4)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 3)),
+                         (p1 | p2 | x1 | board.positions[(7,3)], x2, 2))
+
+    def test_no_east_wrapped_capture(self):
+        p1 = sum(board.positions[x] for x in [(3,3)])
+        p2 = sum(board.positions[x] for x in [(3,1), (3,2)])
+
+        x1 = board.positions[(2,6)]
+        x2 = board.positions[(2,7)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(3,0)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (3, 0)),
+                         (p1 | p2 | x1 | board.positions[(3,0)], x2, 2))
+
+        x1 = board.positions[(3,6)]
+        x2 = board.positions[(3,7)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(3,0)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (3, 0)),
+                         (p1 | p2 | x1 | board.positions[(3,0)], x2, 2))
+
+        x1 = board.positions[(4,6)]
+        x2 = board.positions[(4,7)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(3,0)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (3, 0)),
+                         (p1 | p2 | x1 | board.positions[(3,0)], x2, 2))
+
+    def test_no_northeast_wrapped_capture(self):
+        p1 = sum(board.positions[x] for x in [(3,6)])
+        p2 = sum(board.positions[x] for x in [(1,4), (2,5)])
+
+        x1 = board.positions[(6,0)]
+        x2 = board.positions[(7,1)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 3)),
+                         (p1 | p2 | x1 | board.positions[(0,3)], x2, 1))
+
+        x1 = board.positions[(6,1)]
+        x2 = board.positions[(7,2)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 3)),
+                         (p1 | p2 | x1 | board.positions[(0,3)], x2, 2))
+
+        x1 = board.positions[(6,2)]
+        x2 = board.positions[(7,3)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 3)),
+                         (p1 | p2 | x1 | board.positions[(0,3)], x2, 2))
+
+    def test_no_northwest_wrapped_capture(self):
+        p1 = sum(board.positions[x] for x in [(3,1)])
+        p2 = sum(board.positions[x] for x in [(1,3), (2,2)])
+
+        x1 = board.positions[(6,5)]
+        x2 = board.positions[(7,4)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0,4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 4)),
+                         (p1 | p2 | x1 | board.positions[(0,4)], x2, 2))
+
+        x1 = board.positions[(6,6)]
+        x2 = board.positions[(7,5)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0,4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 4)),
+                         (p1 | p2 | x1 | board.positions[(0,4)], x2, 2))
+
+        x1 = board.positions[(6,7)]
+        x2 = board.positions[(7,6)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(0,4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (0, 4)),
+                         (p1 | p2 | x1 | board.positions[(0,4)], x2, 1))
+
+    def test_no_southwest_wrapped_capture(self):
+        p1 = sum(board.positions[x] for x in [(4,1)])
+        p2 = sum(board.positions[x] for x in [(5,2), (6,3)])
+
+        x1 = board.positions[(1,5)]
+        x2 = board.positions[(0,4)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 4)),
+                         (p1 | p2 | x1 | board.positions[(7,4)], x2, 2))
+
+        x1 = board.positions[(1,6)]
+        x2 = board.positions[(0,5)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 4)),
+                         (p1 | p2 | x1 | board.positions[(7,4)], x2, 2))
+
+        x1 = board.positions[(1,7)]
+        x2 = board.positions[(0,6)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,4)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 4)),
+                         (p1 | p2 | x1 | board.positions[(7,4)], x2, 1))
+
+    def test_no_southeast_wrapped_capture(self):
+        p1 = sum(board.positions[x] for x in [(4,6)])
+        p2 = sum(board.positions[x] for x in [(5,5), (6,4)])
+
+        x1 = board.positions[(1,0)]
+        x2 = board.positions[(0,1)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 3)),
+                         (p1 | p2 | x1 | board.positions[(7,3)], x2, 1))
+
+        x1 = board.positions[(1,1)]
+        x2 = board.positions[(0,2)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 3)),
+                         (p1 | p2 | x1 | board.positions[(7,3)], x2, 2))
+
+        x1 = board.positions[(1,2)]
+        x2 = board.positions[(0,3)]
+        self.assertEqual(board.legal_plays((p1 | x1, p2 | x2, 1)), [(7,3)])
+        self.assertEqual(board.play((p1 | x1, p2 | x2, 1), (7, 3)),
+                         (p1 | p2 | x1 | board.positions[(7,3)], x2, 2))
+
+
+if __name__ == '__main__':
+    unittest2.main()
