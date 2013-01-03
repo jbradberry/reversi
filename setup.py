59e37b528d798ce653319d7a07f7d819149c41cf
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-01-03 15:10:58 -0500

    Filled in a simple setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Reversi',
+    version='0.1dev',
+    author='Jeff Bradberry',
+    author_email='jeff.bradberry@gmail.com',
+    packages=['reversi'],
+    license='LICENSE',
+    description="An implementation of the board game Reversi.",
+)
