e58fd1e14e3690e71dba7a96f9b653caedfb0f23
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-09-02 20:33:33 -0400

    Fixed the board entry point.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='jeff.bradberry@gmail.com',
     packages=['reversi'],
     entry_points={
-        'jrb_board.games': 'reversi.reversi:Board',
+        'jrb_board.games': 'reversi = reversi.reversi:Board',
     },
     license='LICENSE',
     description="An implementation of the board game Reversi.",
